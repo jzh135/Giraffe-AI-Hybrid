f604e02f8c3a1a52af6ea23c0050b33ab6b40c61
jzh135 <163363154+jzh135@users.noreply.github.com>
2024-03-23 19:15:48 -0400

    Add directory checker and import OpenAI API key from a file

--- constants.py
+++ constants.py
@@ -1,14 +1,37 @@
 import os
 
-# Source Document Directory
-SOURCE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'source')
-SINGLE_SOURCE = os.path.join(os.path.dirname(__file__), 'source', 'Severe mental illness across cultures.pdf') 
+# Import OpenAI API Key from a file
+def read_api_key_from_file(file_path):
+    try:
+        with open(file_path, "r") as file:
+            api_key = file.read().strip()  # Read the content and remove leading/trailing spaces
+            return api_key
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
 
+# Document Directory
+SOURCE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'source')
+
+# Audio Directory
+AUDIO_ROOT = os.path.join(os.path.dirname(__file__), 'audio_docs')
+AUDIO_SOURCE = os.path.join(AUDIO_ROOT, 'audio_source')
+TRANSCRIPTION = os.path.join(AUDIO_ROOT, 'transcription')
 # Database Directory
 DB_DIRECTORY = os.path.join(os.path.dirname(__file__), 'db_en')
 
 # Embeddings Directory
-BEG_EN = os.path.join(os.path.dirname(__file__), 'model','bge-large-en-v1.5')
+MODEL_ROOT = os.path.join(os.path.dirname(__file__), 'model')
+BEG_EN = os.path.join(MODEL_ROOT,'bge-large-en-v1.5')
 
 # OpenAI API Key 
-API_KEY = r"Your API Key"
+API_KEY_FILE = os.path.join(os.path.dirname(__file__), 'openAI_API.txt')
+API_KEY = read_api_key_from_file(API_KEY_FILE)
+
+# Check if essential directories exist
+for folder_path in [SOURCE_DIRECTORY, AUDIO_ROOT, AUDIO_SOURCE, TRANSCRIPTION, DB_DIRECTORY, MODEL_ROOT]:
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created successfully.")
+    else:
+        pass
