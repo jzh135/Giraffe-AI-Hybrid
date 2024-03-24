bfca834eea04575d75dafc15ef8103a1d98b14b6
jzh135 <163363154+jzh135@users.noreply.github.com>
2024-03-23 21:25:06 -0400

    Update constants.py
    Support DB update

--- constants.py
+++ constants.py
@@ -12,7 +12,9 @@ def read_api_key_from_file(file_path):
 
 # Document Directory
 SOURCE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'source')
-
+ARCHIVE_DIRECTORY = os.path.join(os.path.dirname(__file__), 'archive')
+METADATA_DIRECTORY = os.path.join(ARCHIVE_DIRECTORY, 'metadata')
+ARCHIVE_SOURCE_DIRECTORY = os.path.join(ARCHIVE_DIRECTORY, 'source files')
 # Audio Directory
 AUDIO_ROOT = os.path.join(os.path.dirname(__file__), 'audio_docs')
 AUDIO_SOURCE = os.path.join(AUDIO_ROOT, 'audio_source')
@@ -29,9 +31,10 @@ API_KEY_FILE = os.path.join(os.path.dirname(__file__), 'openAI_API.txt')
 API_KEY = read_api_key_from_file(API_KEY_FILE)
 
 # Check if essential directories exist
-for folder_path in [SOURCE_DIRECTORY, AUDIO_ROOT, AUDIO_SOURCE, TRANSCRIPTION, DB_DIRECTORY, MODEL_ROOT]:
+for folder_path in [SOURCE_DIRECTORY, ARCHIVE_DIRECTORY, METADATA_DIRECTORY, ARCHIVE_SOURCE_DIRECTORY, AUDIO_ROOT, AUDIO_SOURCE, TRANSCRIPTION, DB_DIRECTORY, MODEL_ROOT]:
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
         print(f"Folder '{folder_path}' created successfully.")
     else:
         pass
+
